d448645dca2c7fba5435f99c3b86ebc6daa7103a
Ganpurev99 <ganpurev.d@ebazaar.mn>
2024-10-01 11:26:56 +0800

    payment app cron section update

--- service/src/app.ts
+++ service/src/app.ts
@@ -40,7 +40,7 @@ app.use(errorHandler);
 cron.schedule("*/10 * * * *", async () => {
   try {
     console.log("Running scheduled task to update QPay token");
-    await axios.get(`${process.env.BASE_URL}${apiPrefix}/qpay/token`);
+    await axios.get(`http://localhost:3000${apiPrefix}/qpay/token`);
     console.log("QPay token updated successfully");
   } catch (error) {
     console.error("Error updating QPay token:", error);
